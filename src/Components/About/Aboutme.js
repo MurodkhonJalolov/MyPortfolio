49d3695fdfc2161dfa9c143b7e74efcb3c7be6c0
MurodkhonJalolov <murodkhanjalolov2004@gmail.com>
2021-08-10 22:10:29 +0500

    about me started

---
+++ src/Components/About/Aboutme.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import './about.css';
+
+function Aboutme() {
+  return (
+    <Container className="aboutme">
+        <div className="aboutme_top"></div>
+        <img src="" alt="" />
+        <div className="aboutme_bottom"></div>
+    </Container>
+  );
+}
+
+export default Aboutme;
+ 
