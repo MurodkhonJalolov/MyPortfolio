2934aa77c1671029935d24b7afb9a015c3b4703c
MurodkhonJalolov <murodkhanjalolov2004@gmail.com>
2021-08-10 14:09:24 +0500

    itbrain finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar'
 import Header from './Components/Header/Header'
+import About from './Components/About/About'
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
        <Navbar />
        <Header />
+       <About />
     </div>
   );
 }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import {} from 'react-bootstrap'
+import Itbrain from './Itbrains'
+import './about.css';
+
+function About() {
+  return (
+    <div className="about">
+       <Itbrain />
+    </div>
+  );
+}
+
+export default About;
  
